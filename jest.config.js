3e408a52fa3cbca9c628c7ccd00991fdc7fb9c2d
Joshua <joshua@collectivehealth.com>
2018-06-15 17:22:22 -0700

    chore: Add project boilerplate.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const ALWAYS_IGNORE = [
+  '<rootDir>/dist',
+  '/node_modules/'
+];
+
+const EXTENSIONS = ['ts', 'tsx', 'js', 'jsx', 'node'];
+
+module.exports = {
+  testEnvironment: 'node',
+  testRegex: '^.+\\.spec.ts$',
+  testPathIgnorePatterns: ALWAYS_IGNORE,
+  coveragePathIgnorePatterns: ALWAYS_IGNORE,
+  moduleFileExtensions: EXTENSIONS,
+  transform: {
+    [`^.+\\.(${EXTENSIONS.join('|')})$`]: 'babel-jest'
+  }
+};
