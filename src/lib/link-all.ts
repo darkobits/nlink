0c2df86d5f19c6237e1bdcbb081db12dba91a151
Joshua <joshua@collectivehealth.com>
2019-01-10 16:22:01 -0800

    feat: Fall-back to attempting an explicit link when no dependencies match.

--- src/lib/link-all.ts
+++ src/lib/link-all.ts
@@ -2,8 +2,36 @@ import chalk from 'chalk';
 import execa from 'execa';
 import findUp from 'find-up';
 import minimatch from 'minimatch';
+// @ts-ignore
+import validateNpmPackageName from 'validate-npm-package-name';
 
 import log from 'lib/log';
+
+
+/**
+ * Returns a flat list of all dependencies found in a project's
+ * package-lock.json.
+ */
+function getDependenciesFromLockfile(cwd: string = process.cwd()): Array<string> {
+  const lockfilePath = findUp.sync('package-lock.json', {cwd});
+
+  if (!lockfilePath) {
+    return [];
+  }
+
+  const {dependencies} = require(lockfilePath);
+
+  return Object.keys(dependencies);
+}
+
+
+/**
+ * Provided a string, returns true if the string is a valid NPM package name.
+ */
+function isValidPackageName(name: string): boolean {
+  return validateNpmPackageName(name).validForNewPackages;
+}
+
 
 /**
  * Replacement for 'npm link <package>' that can match multiple packages using
@@ -20,19 +48,26 @@ export default function linkAll(pattern: string, userOpts: any = {}) {
     log.info('', chalk.dim('Performing a dry run.'));
   }
 
-  const lockfilePath = findUp.sync('package-lock.json', {cwd: opts.cwd});
+  // const lockfilePath = findUp.sync('package-lock.json', {cwd: opts.cwd});
 
-  if (!lockfilePath) {
-    throw new Error('Unable to find "package-lock.json".');
-  }
+  // if (!lockfilePath) {
+  //   throw new Error('Unable to find "package-lock.json".');
+  // }
 
-  const {dependencies} = require(lockfilePath);
+  // const {dependencies} = require(lockfilePath);
 
-  const packagesToLink = Object.keys(dependencies).filter(minimatch.filter(pattern));
+  const dependencies = getDependenciesFromLockfile(opts.cwd);
+
+  const packagesToLink = dependencies.filter(minimatch.filter(pattern));
 
   if (!packagesToLink.length) {
-    log.warn('', `${chalk.dim('Zero dependencies matched pattern:')} ${pattern}`);
-    return;
+    if (isValidPackageName(pattern)) {
+      log.verbose('', chalk.dim('Input did not match any dependencies, but is a valid package name; treating as explicit.'));
+      packagesToLink.push(pattern);
+    } else {
+      log.error('', chalk.dim('Input did not match any dependencies and is not a valid package name; aborting.'));
+      return;
+    }
   }
 
   packagesToLink.forEach(pkg => {
